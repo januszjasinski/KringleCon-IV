a1549a531d04571669b12771b61caa46f61b7003
MrJ <janusz.jasinski@gmail.com>
2021-12-29 13:30:15 +0000

    Create level07.py

---
+++ Elf Code Python/level07.py
@@ -0,0 +1,8 @@
+import elf, munchkins, levers, lollipops, yeeters, pits
+for num in range(2): #not sure if number is right
+    elf.moveLeft(2)
+    elf.moveUp(12)
+    elf.moveLeft(2)
+    elf.moveDown(12)
+elf.moveLeft(3)
+elf.moveUp(100)
