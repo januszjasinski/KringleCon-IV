130b4da2ce73ad5cbee0ceab985bb5bbb2f85e38
MrJ <janusz.jasinski@gmail.com>
2021-12-29 13:28:41 +0000

    Create level02.py

---
+++ Elf Code Python/level02.py
@@ -0,0 +1,8 @@
+import elf, munchkins, levers, lollipops, yeeters, pits
+all_lollipops = lollipops.get()
+lollipop1 = all_lollipops[1]
+lollipop0 = all_lollipops[0]
+elf.moveTo(lollipop1.position)
+elf.moveTo(lollipop0.position)
+elf.moveLeft(3)
+elf.moveUp(100)
