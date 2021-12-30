aea718411768dbe3c56c5ac4a8b49728556c671c
MrJ <janusz.jasinski@gmail.com>
2021-12-30 14:36:49 +0000

    Solution to the Yara Analysis terminal
    Just needs to run the application at the end

--- YaraAnalysis.sh
+++ YaraAnalysis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 perl -pi -e 's/candycane/c4ndyc4ne/g' ./the_critical_elf_app
 perl -pi -e 's/\x72\x6f\x67\x72\x61\x6d\x21\x21/\x72\x6f\x67\x72\x61\x6d\x21\x20/g' ./the_critical_elf_app 
 perl -pi -e 's/__frame_dummy_init_array_entry/__fr4me_dummy_init_array_entry/g' ./the_critical_elf_app
