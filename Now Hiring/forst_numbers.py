4a32cdd7602a2109e2c7e7a3c36a6aef394241cc
MrJ <janusz.jasinski@gmail.com>
2022-01-06 15:18:17 +0000

    Update forst_numbers.py

--- Now Hiring/forst_numbers.py
+++ Now Hiring/forst_numbers.py
@@ -2,7 +2,7 @@ import requests
 import hashlib
 
 # Making a GET request
-for x in range(4999,10000):
+for x in range(0,10000):
         r = requests.get('https://apply.jackfrosttower.com/images/'+str(x)+'.jpg')
 
 # check status code for response received
