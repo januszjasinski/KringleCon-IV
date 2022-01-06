f79665d4eeece63d0b5f1552d3276d85a94e6354
MrJ <janusz.jasinski@gmail.com>
2022-01-06 15:19:00 +0000

    Update forst_numbers.py

--- Now Hiring/forst_numbers.py
+++ Now Hiring/forst_numbers.py
@@ -4,10 +4,7 @@ import hashlib
 # Making a GET request
 for x in range(0,10000):
         r = requests.get('https://apply.jackfrosttower.com/images/'+str(x)+'.jpg')
-
-# check status code for response received
-# success code - 200
-        print(x)
+        
         md5_value = hashlib.md5(r.text.encode('utf-8'))
         with open(md5_value.hexdigest()+".txt", "w") as f:
                 f.write(r.text)
